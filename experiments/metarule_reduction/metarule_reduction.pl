e87154ed554b6389ca3d07fa5315d8e63826e5cc
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2021-04-04 00:44:00 +0300

    Tweaked logging.

--- experiments/metarule_reduction/metarule_reduction.pl
+++ experiments/metarule_reduction/metarule_reduction.pl
@@ -32,7 +32,7 @@ metarule_variations(K,K,_T,_L,_Ts,_Hs,_M,_S,Acc,Rs):-
         reverse(Acc,Rs)
         ,!.
 metarule_variations(I,K,T,L,Ts,Hs,M,S,Acc,Bind):-
-        debug(progress,'Step ~w of ~w',[I,K])
+        debug(progress,'(~w) Step ~w of ~w',[T,I,K])
         ,metarule_subset_evaluations(T,L,Ts,Hs,M,S,Vs)
         ,succ(I,I_)
         ,metarule_variations(I_,K,T,L,Ts,Hs,M,S,[Vs|Acc],Bind).
@@ -40,7 +40,7 @@ metarule_variations(I,K,T,L,Ts,Hs,M,S,Acc,Bind):-
 
 metarule_subset_evaluations(T,L,[Pos,Neg,BK,MS],Hs,M,S,Vs):-
         random_permutation(MS,MS_)
-        ,debug(progress,'Random permutation of MS: ~w',[MS])
+        ,debug(progress,'(~w) Random permutation of MS: ~w',[T,MS])
         ,metarule_subset_evaluations(T,L,[Pos,Neg,BK,MS_],Hs,M,S,[],Vs).
 
 metarule_subset_evaluations(T,L,[Pos,Neg,BK,[]],Hs,M,S,Acc,Vs):-
@@ -54,14 +54,14 @@ metarule_subset_evaluations(T,L,[Pos,Neg,BK,MS],Hs,M,S,Acc,Bind):-
 
 
 metarule_subset_evaluation(T,L,[Pos,Neg,BK,MS],Hs,M,S,N,V):-
-        debug(progress,'User-defined metarules: ~w',[MS])
+        debug(progress,'(~w) User-defined metarules: ~w',[T,MS])
         ,learn_meta_reduction(T,L,[Pos,Neg,BK,MS],Hs,M,S,Ps,V)
-        ,debug(progress,'Measured ~w: ~w',[M,V])
-        ,debug(metarule_reduction_full,'Measured ~w: ~w sec',[M,V])
+        ,debug(progress,'(~w) Measured ~w: ~w',[T,M,V])
+        ,debug(metarule_reduction_full,'(~w) Measured ~w: ~w sec',[T,M,V])
         ,debug_clauses(metarule_reduction_full,'Learned:',Ps)
         ,length(Ps, Len)
-        ,debug(progress,'Hypothesis size: ~w',[Len])
-        ,debug(metarule_reduction_full,'Hypothesis size: ~w',[Len])
+        ,debug(progress,'(~w) Hypothesis size: ~w',[T,Len])
+        ,debug(metarule_reduction_full,'(~w) Hypothesis size: ~w',[T,Len])
         ,length(MS, N).
 
 
@@ -70,13 +70,13 @@ learn_meta_reduction(_T,L,[Pos,Neg,BK,MS],[],time,S,Ps,D):-
         ,evaluation:train_test_splits(S,Pos,Pos_Train,_Pos_Test)
         ,evaluation:train_test_splits(S,Neg,Neg_Train,_Neg_Test)
         ,learning_curve:learn_timing([Pos_Train,Neg_Train,BK,MS],L,Ps,D).
-learn_meta_reduction(_T,L,[Pos,Neg,BK,MS],Hs,time,S,Ps,D):-
+learn_meta_reduction(T,L,[Pos,Neg,BK,MS],Hs,time,S,Ps,D):-
         !
         ,evaluation:train_test_splits(S,Pos,Pos_Train,_Pos_Test)
         ,evaluation:train_test_splits(S,Neg,Neg_Train,_Neg_Test)
 
         % The output of this will need to be combined with MS below.
-        ,debug(progress,'Learning new metarules: ~w',[MS])
+        ,debug(progress,'(~w) Learning new metarules specialising: ~w',[T,Hs])
         ,learn_metarules(Pos,Neg,BK,Hs,MS_1)
         ,expanded_metarules(MS,MS_e)
         ,combined(MS_1,MS_e,MS_)
@@ -85,7 +85,7 @@ learn_meta_reduction(T,L,[Pos,Neg,BK,MS],[],M,S,Ps,V):-
         !
         ,once(timed_train_and_test(T,S,L,[Pos,Neg,BK,MS],Ps,M,V)).
 learn_meta_reduction(T,L,[Pos,Neg,BK,MS],Hs,M,S,Ps,V):-
-        debug(progress,'Learning new metarules: ~w',[MS])
+        debug(progress,'(~w) Learning new metarules specialising: ~w',[T,Hs])
         ,learn_metarules(Pos,Neg,BK,Hs,MS_1)
         ,expanded_metarules(MS,MS_e)
         ,combined(MS_1,MS_e,MS_)
@@ -94,7 +94,7 @@ learn_meta_reduction(T,L,[Pos,Neg,BK,MS],Hs,M,S,Ps,V):-
 
 combined(MS_1,MS_2,MS):-
         flatten([MS_1,MS_2],MS_)
-        ,debug_quantified_metarules(progress,'All metarules:',MS_)
+        ,debug_quantified_metarules(metarule_reduction,'All metarules:',MS_)
         ,copy_term(MS_,MS_c)
         ,findall(H-B
                 ,(member(H:-B,MS_c)
@@ -107,7 +107,9 @@ combined(MS_1,MS_2,MS):-
                 ,(member(H-B,MS_v)
                  )
                 ,MS)
-        ,debug_quantified_metarules(progress,'Combined metarules:',MS).
+        ,length(MS,N)
+        ,debug_quantified_metarules(metarule_reduction,'Combined metarules:',MS)
+        ,debug(progress,'Combined metarules: ~w',[N]).
 
 
 %!	log_experiment_setup(+Target,+Limit,+Metric,+Steps,+Samples) is
